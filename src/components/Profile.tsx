9f8e4d5d345623a6d38c8418624f869d2b85cd81
eduardonk9999 <thiago.novak7@gmail.com>
2021-03-16 16:03:38 -0300

    Add, pag Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/eduardonk9999.png" alt=""/>
+      <div>
+        <strong>Eduardo Silva</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
